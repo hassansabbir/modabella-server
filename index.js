20eca9a16213262034592bc0bf269fb12b7ef081
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-07-01 16:58:31 +0600

    cart info get by email

--- index.js
+++ index.js
@@ -50,6 +50,17 @@ async function run() {
     });
 
     //cartCollection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
