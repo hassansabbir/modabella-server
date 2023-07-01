73ecc4f93d37122cfa2a78df3022c1d57a567e4e
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-07-01 13:44:54 +0600

    make an admin added

--- index.js
+++ index.js
@@ -74,6 +74,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
